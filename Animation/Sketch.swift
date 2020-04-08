7e53ca10a7a6b5d5c3ee9935d323908715f20b7c
Sebastian Tyson <styson@lcs.on.ca>
2020-04-08 14:23:40 -0400

    Got a blue circle to move and leave a trail behind.

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -24,8 +24,17 @@ class Sketch : NSObject {
     // This function runs repeatedly, forever, to create the animated effect
     func draw() {
         
+        // Change fill to white
+        canvas.fillColor = Color(hue: 0, saturation: 0, brightness: 100, alpha: 10)
+        
+        // Draw a square
+        canvas.drawRectangle(at: Point(x: 0, y: 0), width: canvas.width, height: canvas.height)
+        
         // Change position
         x += 1
+        
+        // Change the fill colour
+        canvas.fillColor = Color.blue
         
         // Draw an ellipse in the middle of the canvas
         canvas.drawEllipse(at: Point(x: x, y: 250), width: 50, height: 50)
