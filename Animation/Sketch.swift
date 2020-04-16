c539a79290acf81c5eee059ca0c31440373dd2c9
Sebastian Tyson <styson@lcs.on.ca>
2020-04-16 13:56:24 -0400

    Made the inverse of the scottish flag, had four lines go from the centre to the corners

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -10,8 +10,6 @@ class Sketch : NSObject {
     // Position of circle
     var x : Int
     var y : Int
-    var a : Int
-    var b : Int
     
     // This function runs once
     override init() {
@@ -21,27 +19,32 @@ class Sketch : NSObject {
         
         // Set starting position
         x = 250
-        
+        y = 250
     }
     
     // This function runs repeatedly, forever, to create the animated effect
     func draw() {
         
         // Change fill to white
-        canvas.fillColor = Color(hue: 0, saturation: 0, brightness: 100, alpha: 10)
+        canvas.fillColor = Color(hue: 0, saturation: 0, brightness: 100, alpha: 0)
+        canvas.drawShapesWithBorders = false
         
         // Draw a square
         canvas.drawRectangle(at: Point(x: 0, y: 0), width: canvas.width, height: canvas.height)
         
         // Change position
         x += 1
+        y -= 1
         
         
         // Change the fill colour
         canvas.fillColor = Color.blue
         
         // Draw an ellipse in the middle of the canvas
-        canvas.drawEllipse(at: Point(x: x, y: 250), width: 50, height: 50)
+        canvas.drawEllipse(at: Point(x: x, y: x), width: 50, height: 50)
+        canvas.drawEllipse(at: Point(x: y, y: x), width: 50, height: 50)
+        canvas.drawEllipse(at: Point(x: y, y: y), width: 50, height: 50)
+        canvas.drawEllipse(at: Point(x: x, y: y), width: 50, height: 50)
         
     }
     
